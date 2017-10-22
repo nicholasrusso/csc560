799b7273af459de4146d867db6d18c0599d1c116
dgirerd <danielgirerd@gmail.com>
2017-10-22 13:05:04 -0700

    Create train.py

---
+++ train.py
@@ -0,0 +1,8 @@
+# For each query
+  # Add to counter for referenced tables
+
+# For each set of table with counters above threshold 
+  # Create MV
+  # Create index
+  # Add MV to our stored database model
+
