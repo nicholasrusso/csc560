b2891b8125bcbba934e5114860371df95276e6de
Sam Hsu <samanthahsu13@gmail.com>
2017-10-31 13:31:00 -0700

    continuing work on trainer

--- train.py
+++ train.py
@@ -1,34 +1,60 @@
 # For each query
-  # Add to counter for referenced tables - tables in Join clause or tables list?
+  # Add to counter for referenced tables
 
-# For each set of table with counters above threshold - set of tables? does the dictionary need to keep track of what tables go together?
+# For each set of table with counters above threshold
   # Create MV
   # Create index
   # Add MV to our stored database model
 
+# Tester needs MV name, columns, tables
+
 from parser import Query
 
+class MaterializedView:
+    def _init_(self, queryStr):
+        self.name =
+        self.selectStr =
+        self.columns =
+        self.tables =
+
+
 def create_mv(tables):
-    table_name = '_'.join(tables) # wouldn't this just be whatever table name I want or?
-    data_query = "SELECT * FROM " + ','.join(tables) # is join a real function or do I need to execute a query that joins the tables?
-    create_query = "CREATE MATERIALIZED VIEW " + table_name + " AS " + data_query
+    view_name = '_'.join(tables)
+    data_query = "SELECT * FROM " + ','.join(tables)
+    create_query = "CREATE MATERIALIZED VIEW " + view_name + " AS " + data_query
     # execute query
-    # add MV to database model
+    # add MV to database model & output for tester?
 
 
-def create_index(tables):
-    create_query = "CREATE INDEX " + index_name + "ON " + table_name # index on column name?
+# should indexes be made on each table in the set of tables being joined?
+def create_index(table):
+    index_name = table + '_index'
+    create_query = "CREATE INDEX " + index_name + "ON " + table # index on column name?
     # execute query
 
 
+# given a query model, for every set of tables (being joined), add to counter dict for tables being joined
 def process_model(queryModel):
+    # queryModel.joinClause.joins = list of JoinOps(leftTable, leftCol, rightTable, rightCol, joinOperator)
+    joins = queryModel.joinClause.joins
     tableCounts = {}
-    for i in range(len(queryModel.tables)): # do we care
-        if (queryModel[i] not in tableCounts):
-            tableCounts[queryModel[i]] = 1
+
+    for i in range(len(queryModel.joinClause.joins)):
+        leftTable = joins[i].leftTable
+        rightTable = joins[i].rightTable
+
+        if (leftTable, rightTable) not in tableCounts:
+            tableCounts[(leftTable, rightTable)] = 1
         else:
-            curCount = tableCounts[queryModel[i]]
-            tableCounts[queryModel[i]] = curCount + 1
+            curCount = tableCounts[(leftTable, rightTable)]
+            tableCounts[(leftTable, rightTable)] = curCount + 1
 
     return tableCounts
 
+
+def createViews(tableCounts, threshold):
+    for tableSet, count in tableCounts.items():
+        if count > threshold:
+            create_mv(list(tableSet)) # convert to list for str.join()
+            for table in tableSet:
+                create_index(table)
