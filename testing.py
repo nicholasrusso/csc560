680c6b97df51ec0d6469b65526f68f03a883a2df
dgirerd <danielgirerd@gmail.com>
2017-11-28 17:08:06 +0000

    more work on testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,12 @@
 # Input to me is a list of MV objects. Each MV object should have name, string query, list of table/column pairs.
+class MaterializedView:
+    def __init__(self, view_name, select_str, tableSets):
+        self.name = view_name
+        self.selectStr = select_str
+        self.tableCols = tableSets
 
+    def __str__(self):
+        return self.name + ' = ' + self.selectStr + ' with ' + str(self.tableCols)
 
 # Disclaimer: only some functions tested
 def get_test_queries(file_name):
@@ -20,14 +27,15 @@ def modify_queries(test_queries):
 
 def is_in_mv(mvs,table,col):
   for mv in mvs:
-    if mv.fields.contains((table,col)):
+    if mv.tableCols.contains((table,col)):
       return true
-    return false
+  return false
 
 def parse_query(query):
   tables_cols = []
-  for word in query:
+  for word in query.split(' '):
     parts = word.split('.')
+    print(parts)
     if len(parts) == 2:
       tables_cols.append((parts[0],parts[1]))
   return tables_cols
@@ -43,5 +51,7 @@ def run_queries(queries):
 
 queries = get_test_queries("testing_queries.txt")
 print(queries)
-modified_queries = modify_queries(queries)
-print(modified_queries)
+#modified_queries = modify_queries(queries)
+#print(modified_queries)
+for query in queries:
+    print(parse_query(query))
