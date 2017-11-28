2ae418190f906e94ace66f8deca11312f0ead8a1
dgirerd <danielgirerd@gmail.com>
2017-11-28 16:06:25 +0000

    work on testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
 # Input to me is a list of MV objects. Each MV object should have name, string query, list of table/column pairs.
 
-# Disclaimer: more psuedo-code than real code. Haven't actually run anything.
+
+# Disclaimer: only some functions tested
 def get_test_queries(file_name):
   with open(file_name) as f:
-    test_queries = f.readlines()
+    test_queries = f.read().splitlines()
   return test_queries
   
 def modify_queries(test_queries):
@@ -12,25 +13,35 @@ def modify_queries(test_queries):
     editable_query = query
     tables_cols = parse_query(query)
     for (table,col) in tables_cols:
-      if(is_in_mv(mvs,table,col):
+      if(is_in_mv(mvs,table,col)):
          editable_query.replace(table+'.'+col, mv.name+'.'+table+'.'+col)
-    modified queries += editable_query
+    modified_queries.append(editable_query)
   return modified_queries
 
 def is_in_mv(mvs,table,col):
   for mv in mvs:
     if mv.fields.contains((table,col)):
       return true
+    return false
 
 def parse_query(query):
   tables_cols = []
   for word in query:
     parts = word.split('.')
     if len(parts) == 2:
-      tables_cols += (parts[0],parts[1])
+      tables_cols.append((parts[0],parts[1]))
   return tables_cols
 
 def run_queries(queries):
   with database("testdb", "test", "test") as db:
     for query in queries:
       db.execute(query) # How are we doing timing?
+
+
+
+
+
+queries = get_test_queries("testing_queries.txt")
+print(queries)
+modified_queries = modify_queries(queries)
+print(modified_queries)
