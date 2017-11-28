0127949bc115a0e65f9f9d99dacf07a351a9d458
dgirerd <danielgirerd@gmail.com>
2017-11-28 17:30:15 +0000

    further work on testing.py

--- testing.py
+++ testing.py
@@ -1,12 +1,9 @@
 # Input to me is a list of MV objects. Each MV object should have name, string query, list of table/column pairs.
-class MaterializedView:
-    def __init__(self, view_name, select_str, tableSets):
-        self.name = view_name
-        self.selectStr = select_str
-        self.tableCols = tableSets
 
-    def __str__(self):
-        return self.name + ' = ' + self.selectStr + ' with ' + str(self.tableCols)
+def run_testing(file_name, mvs):
+  test_queries = get_test_queries(file_name)
+  modified_queries(test_queries, mvs)
+  run_queries(modified_queries)
 
 # Disclaimer: only some functions tested
 def get_test_queries(file_name):
@@ -14,7 +11,7 @@ def get_test_queries(file_name):
     test_queries = f.read().splitlines()
   return test_queries
   
-def modify_queries(test_queries):
+def modify_queries(test_queries, mvs):
   modified_queries = []
   for query in test_queries:
     editable_query = query
@@ -25,7 +22,7 @@ def modify_queries(test_queries):
     modified_queries.append(editable_query)
   return modified_queries
 
-def is_in_mv(mvs,table,col):
+def is_in_mv(mvs, table, col):
   for mv in mvs:
     if mv.tableCols.contains((table,col)):
       return true
@@ -46,12 +43,3 @@ def run_queries(queries):
       db.execute(query) # How are we doing timing?
 
 
-
-
-
-queries = get_test_queries("testing_queries.txt")
-print(queries)
-#modified_queries = modify_queries(queries)
-#print(modified_queries)
-for query in queries:
-    print(parse_query(query))
