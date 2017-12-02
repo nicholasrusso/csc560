17b47aba03fade2b3228a81ae1051b0c116ac2e9
dgirerd <danielgirerd@gmail.com>
2017-12-02 19:42:23 +0000

    testing comparing tables progress

--- testing.py
+++ testing.py
@@ -1,40 +1,53 @@
 # Input to me is a list of MV objects. Each MV object should have name, string query, list of table/column pairs.
 import pickle
+from train import MaterializedView
 
-def run_testing(test_queries, mvs):
-  modified_queries(test_queries, mvs)
-  run_queries(modified_queries)
 
 def modify_queries(test_queries, mvs):
-#need to modify for test_queries being parser format
   modified_queries = []
+# get list of the tables in the join
+# see if an mv exactly matches that list of tables
+# call tablename replacement tablename
+  print("modify_queries")
+
   for query in test_queries:
-    editable_query = query
-    tables_cols = parse_query(query)
-    for (table,col) in tables_cols:
-      if(is_in_mv(mvs,table,col)):
-         editable_query.replace(table+'.'+col, mv.name+'.'+table+'.'+col)
-    modified_queries.append(editable_query)
+    print(query.tables)
+    for mv in mvs:
+        if match_mv(mv, query.tables):
+            #print("match")
+            continue
   return modified_queries
 
-def is_in_mv(mvs, table, col):
+def match_mv(mv, tables):
+  for (table,col) in mv.tableCols:
+    if table not in tables:
+      return False
+  return True
+
+def is_in_mvs(mvs, table, col):
   for mv in mvs:
     if mv.tableCols.contains((table,col)):
       return true
   return false
 
-def parse_query(query):
-  tables_cols = []
-  for word in query.split(' '):
-    parts = word.split('.')
-    print(parts)
-    if len(parts) == 2:
-      tables_cols.append((parts[0],parts[1]))
-  return tables_cols
+def run_testing(test_queries, mvs):
+    print("MVs");
+    print(str(mvs[0]))
+    print(str(mvs[1]))
+    print(str(mvs[2]))
+    print(str(mvs[3]))
+    print("test_queries")
+    print(test_queries[0])
+    print(test_queries[0])
+    print(test_queries[0])
+    print(test_queries[0])
+    modify_queries(test_queries, mvs)
+#  run_queries(modified_queries)
 
 def save_queries(queries):
   string_queries = []
   for query in queries:
     string_queries.append(query.toString())
 
-run_testing(pivkel.load(open("testing_queries.pickle","rb"),pickle.load(open("mvs.pickle","rb")))
+run_testing(pickle.load(open("queries.pickle","rb")),pickle.load(open("MVs.p","rb")))
+
